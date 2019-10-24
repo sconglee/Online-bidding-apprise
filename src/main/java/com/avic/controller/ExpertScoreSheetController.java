08c58e3a0d5828dc1a0a63626ec1ad437e5c1e84
sconglee <sconglee@163.com>
2019-10-24 14:22:00 +0800

    normal

--- src/main/java/com/avic/controller/ExpertScoreSheetController.java
+++ src/main/java/com/avic/controller/ExpertScoreSheetController.java
@@ -7,12 +7,16 @@ import com.avic.model.ScoreSheetTemplate;
 import com.avic.service.ExpertScoreSheetService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -31,12 +35,12 @@ public class ExpertScoreSheetController {
     private ExpertScoreSheetService expertScoreSheetService;
 
     /**
-    * @Author xulei
-    * @Description 新增专家打分记录
-    * @Date 16:33 2019/10/18/018
-    * @Param []
-    * @return java.util.Map<java.lang.String,java.lang.Object>
-    **/
+     * @return java.util.Map<java.lang.String       ,       java.lang.Object>
+     * @Author xulei
+     * @Description 新增专家打分记录
+     * @Date 16:33 2019/10/18/018
+     * @Param []
+     **/
     @RequestMapping(value = "insertExpertScoreSheet")
     @ResponseBody
     public Map<String, Object> insertExpertScoreSheet(ExpertScoreSheet expertScoreSheet) {
@@ -56,12 +60,32 @@ public class ExpertScoreSheetController {
 
         } else {
             modelMap.put("success", "false");
-            modelMap.put("msg","保存专家评标打分结果失败！！");
+            modelMap.put("msg", "保存专家评标打分结果失败！！");
         }
-        
+
         return modelMap;
     }
 
+    @RequestMapping(value = "getallexportscore")
+    public Map<String, Object> getAllExportScore(@Param("projectNumber") String projectNumber) {
+        Map map = new HashMap();
+        if (projectNumber != null && projectNumber.length() != 0) {
+            List<ExpertScoreSheet> expertScoreSheetList = expertScoreSheetService.getAllExpertScoreByProjectNumber(projectNumber);
+            map.put("expertScoreSheetList", expertScoreSheetList);
+            map.put("msg", "查询打分列表成功！");
+        } else {
+            map.put("msg", "无效的项目编号！");
+        }
+        return map;
+    }
 
+    @RequestMapping(value = "getscoreinfo")
+    public Map<String, Object> getScoreInfo(@ModelAttribute("expertScoreSheet") ExpertScoreSheet expertScoreSheet) {
+        Map map = new HashMap();
+        ExpertScoreSheet expertScoreSheet1 = expertScoreSheetService.getScoreInfoByExpertNameAndCompanyName(expertScoreSheet);
+        map.put("expertScoreSheet", expertScoreSheet1);
+        map.put("msg", "查询打分详情信息成功！");
+        return map;
+    }
 
 }
